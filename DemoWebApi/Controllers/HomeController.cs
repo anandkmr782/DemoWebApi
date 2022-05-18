a0be07666ee182ffd15b910b45a5238c8195bc18
Anand kumar <anandkmr782@gmail.com>
2022-05-18 14:47:26 +0530

    Add project files.

---
+++ DemoWebApi/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using DemoWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoWebApi.Controllers
+{
+    public class HomeController : Controller
+    {               
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
